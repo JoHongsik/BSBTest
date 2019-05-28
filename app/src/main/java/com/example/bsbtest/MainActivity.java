791d3354f1ea0df546da59a54efff373782b3d7a
HongsikJo <queue1991@naver.com>
2019-05-28 17:29:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/bsbtest/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.bsbtest;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private LinearLayout bottomSheet;
+    private BottomSheetBehavior bottomSheetBehavior;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomSheet = findViewById(R.id.bottomSheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+                Log.d("chaged!","chaged!" + i);
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+
+            }
+        });
+    }
+}
